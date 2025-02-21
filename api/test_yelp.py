5811076bc5f762c1a6e2c98249312a9b4bd87dc7
ronney221 <ronneydo1@gmail.com>
2025-02-21 13:33:43 -0800

    vercel build debug

--- api/test_yelp.py
+++
@@ -1,7 +0,0 @@
-import os
-os.environ["YELP_API_KEY"] = "your-yelp-api-key-here"
-from yelp import main
-
-args = {"yelpUrl": "https://www.yelp.com/biz/sugo-hand-roll-bar-seattle-2?osq=sugo"}
-result = main(args)
-print(result)
